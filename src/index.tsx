5a90ae56ee7e5cfb0619f35e3d72fde4838fad9b
Gabor Molnar <gabormolnar92@gmail.com>
2022-06-19 23:43:25 +0200

    Init Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+import './index.css'
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000',
-    cache: new InMemoryCache()
-});
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <App/>
-        </ApolloProvider>
-    </React.StrictMode>
-);
-
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+)
